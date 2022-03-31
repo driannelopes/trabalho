bb71508d06296974f55fe20d486f891116d0a359
driannelopes <lopesadrianne80@gmail.com>
2022-03-31 16:19:04 -0300

    trabalho

---
+++ questao_1/index.js
@@ -0,0 +1,9 @@
+var valor = prompt("digite valor")
+
+if(valor > 10  ){
+    document.write( "valor maior que 10"); 
+    console.log("valor:"+ valor)
+}else if( valor < 10){
+    document.write("valor menor que 10");
+    console.log( "valor: " + valor)
+}
